7b651b9da5c30197973d1996a5e8332f47a9ae02
SWE-Jahidul <programmerjahid162@gmail.com>
2021-11-01 05:57:01 -0500

    addiing vedio section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import About from "../About/About";
 import HeaderBanner from "../HeaderBanner/HeaderBanner";
+import Vedio from "../Vedio/Vedio";
 
 const Home = () => {
   return (
     <div>
       <HeaderBanner></HeaderBanner>
       <About></About>
+
+      <Vedio></Vedio>
     </div>
   );
 };
---
+++ src/Components/Vedio/Vedio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Vedio = () => {
+  return (
+      <div>
+
+      </div>
+  )
+};
+
+export default Vedio;
