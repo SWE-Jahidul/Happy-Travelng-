f660f5c903a6624a0423db8e3cfec35e3ad0682d
SWE-Jahidul <programmerjahid162@gmail.com>
2021-11-01 02:11:51 -0500

    ädding router and navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Events from "./Components/Events/Events";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar> </Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+
+          <Route path="/about">
+            <About></About>
+          </Route>
+
+          <Route path="/events">
+            <Events></Events>
+          </Route>
+
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
